adc84b5c93c922b57144974ac1850e125974a2df
Ederson <drsnmelo@gmail.com>
2021-03-29 21:06:54 -0300

    Primeiro commit

---
+++ src/test/java/br/com/korrellia/Clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.korrellia.Clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
